a2b9eb2e22b372001078901d28d779c3d59b59af
Pooja <pthakare33@gmail.com>
2016-11-26 18:38:56 +0530

    fhfgh

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -6,23 +6,38 @@
  */
 var cricapi = require("node-cricapi");
 var request = require('request');
+// NgppjVYBlcNihBRKvsWo7KV1C5R2 
 module.exports = {
-  get: function (req, res) {
-    for (var i = 1; i <= 500; i++) {
-      request('http://cricapi.com/api/playerStats?pid=' + i, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-          console.log(body) // Show the HTML for the Google homepage. 
-          console.log(response.body);
-          var data = JSON.parse(response.body);
-          if (data.name == "") {
+  // getAll: function (req, res) {
+  //   for (var i = 1; i <= 500; i++) {
+  //     request({
+  //       url: 'http://cricapi.com/api/playerStats?pid=1000',
+  //       // apikey: 'NgppjVYBlcNihBRKvsWo7KV1C5R2',
+  //       apikey: '7dn28he8h2e72',
+  //       method: "POST"
+  //     }, function (error, response, body) {
+  //       if (!error && response.statusCode == 200) {
+  //         console.log(body) // Show the HTML for the Google homepage. 
+  //         console.log(response.body);
+  //         var data = JSON.parse(response.body);
+  //         if (data.name == "") {
 
-          } else {
-            res.json(JSON.parse(response.body));
-          }
+  //         } else {
+  //           res.json(JSON.parse(response.body));
+  //         }
 
-        }
-      })
-    }
+  //       }
+  //     })
+  //   }
+  // },
+  insertEach: function (req, res) {
+    request('http://cricapi.com/api/playerStats?pid=4068', function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var data = JSON.parse(response.body);
+        console.log(data) // Show the HTML for the Google homepage. 
+        res.json(JSON.parse(response.body));
+      }
+    })
   },
   add: function (req, res) {
     User.create(req.body).exec(function (err, result) {
